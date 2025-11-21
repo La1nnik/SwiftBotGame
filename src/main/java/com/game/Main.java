37290e5788b87d8c198efeffa02e54ae733197ff
La1nnik <159943235+La1nnik@users.noreply.github.com>
2025-11-21 19:10:15 +0000

    Refactor Main class formatting and add printScore method for round and score display
    Signed-off-by: La1nnik <159943235+La1nnik@users.noreply.github.com>

--- src/main/java/com/game/Main.java
+++ src/main/java/com/game/Main.java
@@ -2,19 +2,41 @@ package com.game;
 
 import swiftbot.SwiftBotAPI;
 
-public class Main {
+public class Main
+{
+
+
+    static void printScore(short round, short score)
+    {
+        System.out.println("\n\u001B[35m===============================\u001B[0m");
+        System.out.println("\u001B[35m    \u001B[35mRound: \u001B[35m" + round + "       Score: " + score + "\u001B[35m   \u001B[0m");
+        System.out.println("\u001B[35m===============================\u001B[0m\n");
+    }
 
 
     static SwiftBotAPI swiftBot;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException
+    {
 
-        try {
+        try
+        {
             swiftBot = SwiftBotAPI.INSTANCE;
-        } catch (Exception e) {
+        }
+
+        catch (Exception e)
+        {
             System.out.println("\nI2C disabled!");
             System.exit(5);
         }
+
+
+        short round = 1;
+        short score = 0;
+
+
+        printScore(round, score);
     }
+
 }
 
